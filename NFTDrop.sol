8c93d792bf303dd0223a2a00a9d80f8ea1dba312
c0smicChef <84823000+c0smicChef@users.noreply.github.com>
2022-07-07 15:07:20 -0400

    Update NFTDrop.sol

--- NFTDrop.sol
+++ NFTDrop.sol
@@ -4,6 +4,16 @@ pragma solidity >=0.7.0 <0.9.0;
  * @title NFTDrop
  * @dev NFT Contract
  */
-contract NFTDrop {
+contract NFTDrop is ERC721A{
+    //whitelist
 
+    //setPrice
+
+    //set royalty
+
+    //do reveal
+
+    //fallback
+
+    //transfer out
 }
