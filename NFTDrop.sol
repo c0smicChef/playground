604cdcd8bd05523f631972bc810d777c6cddf3ed
c0smicChef <84823000+c0smicChef@users.noreply.github.com>
2022-07-20 14:59:19 -0400

    Update NFTDrop.sol

--- NFTDrop.sol
+++ NFTDrop.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.10;
 import "erc721a/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 /**
  * @title NFTDrop
@@ -19,48 +20,46 @@ contract NFTDrop is ERC721A, Ownable, ReentrancyGuard {
     uint256 public salePrice = 0.01 ether;
     string public baseURI;
     bool public revealed;
+    bytes32 public merkleRoot;
 
     constructor() 
         ERC721A("C0smic", "CSMC")
         Ownable()
         ReentrancyGuard() 
     {}
+
     function _baseURI() internal view virtual override returns (string memory) {
         return baseURI;
     }
-    //mint
-    function mint(uint256 amount) public payable{
+
+    function mint(uint256 amount) public payable {
         require(amount > 0, "Amount to mint must be greater than 0");
-        require(msg.value == salePrice * amount, "Invalid Payment amount");
+        require(MerkleProof.verify(merkleRoot, _merkleProof, msg.sender), "Address not in whitelist");
+           
+        //require(msg.value == salePrice * amount, "Invalid Payment amount");
         _safeMint(msg.sender, amount);
     }
 
     //whitelist
+    function setMerkleRoot(bytes32 _merkleRoot) public onlyOwner {
+        merkleRoot = _merkleRoot;
+    }
 
-    //setPrice
-    function setPrice(uint256 newPrice) public onlyOwner
-    {
+    function setPrice(uint256 newPrice) public onlyOwner {
         salePrice = newPrice;
     }
 
-    //set royalty
-
-    //do reveal
     function reveal(string calldata newBaseURI) public onlyOwner {
         require(!revealed, "Already revealed");
         baseURI = newBaseURI;
         revealed = true;
     }
 
-    //fallback
-    receive() external payable
-    {
+    receive() external payable {
         emit FreeEth(msg.sender, string(abi.encodePacked("Thanks for the ETH: ", msg.value)));
     }
 
-    //transfer out
-    function withdrawTo(address to, uint256 amount) public onlyOwner
-    {
+    function withdrawTo(address to, uint256 amount) public onlyOwner {
         require(to != address(0), "Cannot withdraw to 0x0 address");
         if (amount == 0) {  //withdraw full balance if amount = 0
             amount = address(this).balance;
